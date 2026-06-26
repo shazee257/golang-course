3f8213913a494bb807f60f294434f8f0f205b01b
Shahzad <shazee257@gmail.com>
2026-06-26 09:25:38 +0500

    feat: Add customer struct to Order and update manualOrder initialization

--- 16_structs/struct.go
+++ 16_structs/struct.go
@@ -5,11 +5,17 @@ import (
 	"time"
 )
 
+type customer struct {
+	name  string
+	phone string
+}
+
 type Order struct {
 	id        string
 	amount    float32
 	status    string
-	createdAt time.Time
+	createdAt time.Time // nanoseconds precision
+	customer  customer
 }
 
 func (o *Order) updateStatus(status string) {
@@ -29,17 +35,27 @@ func newOrder(id string, amount float32, status string) *Order {
 }
 
 func main() {
-	language := struct {
-		name   string
-		isGood bool
-	}{name: "Go", isGood: true}
-	fmt.Println("language: ", language)
-	fmt.Printf("%+v\n", language) // %+v prints the struct with field names
+	// language := struct {
+	// 	name   string
+	// 	isGood bool
+	// }{name: "Go", isGood: true}
+	// fmt.Println("language: ", language)
+	// fmt.Printf("%+v\n", language) // %+v prints the struct with field names
+
+	// newCustomer := customer{
+	// 	name:  "John Doe",
+	// 	phone: "123-456-7890",
+	// }
 
 	manualOrder := Order{
 		id:     "123",
 		amount: 100.0,
 		status: "pending",
+		// customer: newCustomer,
+		customer: customer{
+			name:  "John Doe",
+			phone: "123-456-7890",
+		},
 	}
 
 	fmt.Println("manualOrder: ", manualOrder)
